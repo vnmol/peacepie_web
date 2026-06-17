cf5df61c9e762a9e5a6ea4bec0340fb27ea5d36e
Vladimir Molodtsov <vmol@mail.ru>
2026-06-17 08:55:19 +0300

    Version_0.3.1.post3

--- config/app_starter_simple_fastapi_dashboard.py
+++ config/app_starter_simple_fastapi_dashboard.py
@@ -35,8 +35,8 @@ class AppStarter:
         name = 'telegram'
         class_desc = {'requires_dist': 'simple_telegram_bot', 'class': 'SimpleTelegramActor'}
         body = {'class_desc': class_desc, 'name': name}
-        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 400)
-        await self.adaptor.ask(self.adaptor.get_msg('start', None, name), 400)
+        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 600)
+        await self.adaptor.ask(self.adaptor.get_msg('start', None, name), 600)
 
     async def simple_fastapi_dashboard(self):
         name = 'dashboard'
