0728e26a67d5895ec96fff1e0302aa3b52f38ba9
Vladimir Molodtsov <vmol@mail.ru>
2026-06-25 08:27:11 +0300

    self_closing_telegram

--- config/app_starter_simple_fastapi_dashboard.py
+++ config/app_starter_simple_fastapi_dashboard.py
@@ -13,16 +13,16 @@ class AppStarter:
         return True
 
     async def start(self):
+        #  await self.telegram()
         #  await self.web_face()
         #  await self.auxiliaries()
-        #  await self.telegram()
         await self.simple_fastapi_dashboard()
         await self.adaptor.send(self.adaptor.get_msg('remove_actor', {'name': self.adaptor.name}))
 
     async def web_face(self):
         name = 'web_face'
         body = {'class_desc': {'requires_dist': 'simple_web_face', 'class': 'SimpleWebFace'}, 'name': name}
-        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 120)
+        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 600)
         body = {'params': [{'name': 'port', 'value': 9090}, {'name': 'is_env_port', 'value': False}]}
         await self.adaptor.ask(self.adaptor.get_msg('set_params', body, name))
         await self.adaptor.ask(self.adaptor.get_msg('start', None, name))
